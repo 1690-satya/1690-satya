f9b1d05e2f199007637a839c64c69ab918a3b3b7
phytoplankton <134634257+1690-satya@users.noreply.github.com>
2023-10-24 00:21:57 +0530

    Update first.c

--- first.c
+++ first.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
- static int x=0;
+ static int x=100;  /* see if we use static keyword then the result is as same as when we use only int x=0 ; But we can't use auto keyword here
+also we can't write static int x ;and then write x=10 ; */
 void f()
 {
   ++x;
